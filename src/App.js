e8f52a1d949ff13592055526081d38fdcff18d07
Sergio Calvo <sergiocami84@gmail.com>
2025-11-02 21:51:20 +0100

    Iimplement character fetching in App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.sampleapis.com/futurama/characters")
+      .then((response) => response.json()) //Cualquier cosa devuelta de la API se guarda en response
+      .then((users) =>
+        this.setState(() => {
+          return { characters: users };
+        },
+        ()=>{
+          console.log(this.state)
+        }
+      )
+      ); //Cualquier cosa devuulta por el then anterior se guardará en users
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>My favourite characters of Futurama</h1>
+          {this.state.characters.map((c) => (
+            <div key={c.id}>
+              <h3>
+                {c.name.first} {c.name.middle} {c.name.last}
+              </h3>
+              <img src={c.images.main} alt={c.name.first}></img>
+            </div>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
