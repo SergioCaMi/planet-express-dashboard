659849b4cd2c536ea0c4f62e3ef8195462e67c71
Sergio Calvo <sergiocami84@gmail.com>
2025-11-06 10:37:45 +0100

    Add CardList and card styles; refactor rendering to include species and occupation

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,30 @@
 import { Component } from "react";
-
+import './card-list.styles.css';
+import './card.styles.css';
 class CardList extends Component {
-    
   render() {
-    console.log('Props: ', this.props)
+    console.log("Props: ", this.props);
     const { characters } = this.props;
     return (
       <div className="card-list">
-        {characters.map((c) => (
-            <div key={c.id}>
-              <h3>
-                {c.name.first} {c.name.middle} {c.name.last}
-              </h3>
-              <img src={c.images.main} alt={c.name.first}></img>
-            </div>
-          ))}
+        {characters.map((c) => {
+            const { id, species, occupation } = c;
+            const name = `${c.name.first} ${c.name.middle} ${c.name.last}`;
+            const image = c.images.main;
+            return (
+          <div className="card-container" key={id}>
+            <img
+              src={image}
+              alt={name}
+            />
+            <h2>
+              {name}
+            </h2>
+            <p>
+             {species} - {occupation}
+            </p>
+          </div>
+        )})}
       </div>
     );
   }
